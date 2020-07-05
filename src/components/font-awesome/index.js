16766881ecac83a2f3d064204bbdf3bde191f429
Aniket Patel <aniket.r.21102001@gmail.com>
2020-07-05 09:48:09 +0530

    Add footer links

--- src/components/font-awesome/index.js
+++ src/components/font-awesome/index.js
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faBars, faShare } from '@fortawesome/free-solid-svg-icons'
 
-library.add( faBars, faShare)
+library.add( fab,faBars, faShare)
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import style from './footer.module.css'
 
 const Footer = () => {
@@ -7,8 +9,16 @@ const Footer = () => {
 
     return(
         <div className={style.container}>
-        <div>Stay Home.</div>
-        <div>Stay Safe.</div>
+        <div>Developed by Aniket</div>
+        <div>Follow me on</div>
+        <div className={style.fontawesome}>
+        <a href='https://www.instagram.com/an.ik.et_p/'><FontAwesomeIcon icon={['fab', 'instagram']} /></a>
+        <a href='https://github.com/AniketPatel148'><FontAwesomeIcon icon={['fab', 'github']} /></a>
+        <a href='https://www.facebook.com/profile.php?id=100037285100535'><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
+        <a href='https://twitter.com/king_of_chausar'><FontAwesomeIcon icon={['fab', 'twitter']} /></a>
+        
+         </div>
+
         <div>Copyright © {year}</div>
         
         </div>
