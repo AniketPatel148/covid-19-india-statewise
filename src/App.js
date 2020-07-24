36177b00f2d9236e26380e57910646099e87d820
Aniket Patel <aniket.r.21102001@gmail.com>
2020-07-24 18:52:56 +0530

    progressive web app

--- src/App.js
+++ src/App.js
@@ -1,107 +1,116 @@
-import React from 'react';
-import { Cards, Charts, Statepicker, Button, Header, Comparisons, Footer} from './components';
-import styles from  './App.module.css';
-import { fetchData,Order,fetchDataWithComma } from './api';
-import './components/font-awesome/index';
+import React from "react";
+import styles from "./App.module.css";
+import { fetchData, Order, fetchDataWithComma } from "./api";
+import "./components/font-awesome/index";
 
-class App extends React.Component{
+import loadable from "@loadable/component";
+const Cards = loadable(() => import("./components/Cards/Cards"));
+const Charts = loadable(() => import("./components/Charts/Charts"));
+const Statepicker = loadable(() =>
+	import("./components/Statepicker/Statepicker")
+);
+const Button = loadable(() => import("./components/buttons/buttons"));
+const Header = loadable(() => import("./components/header/header"));
+const Comparisons = loadable(() =>
+	import("./components/Comparison/comparison")
+);
+const Footer = loadable(() => import("./components/footer/footer"));
 
-  state = {
-    dataForCharts: {},
-    dataForCards:{},
-    state: '',
-    showChart: false,
-    ButtonName: "Charts",
-    showComparison: false,
-    ButtonName2: "Compare",
-    Order:[],
-    showCard: true
-  }
+class App extends React.Component {
+	state = {
+		dataForCharts: {},
+		dataForCards: {},
+		state: "",
+		showChart: false,
+		ButtonName: "Charts",
+		showComparison: false,
+		ButtonName2: "Compare",
+		Order: [],
+		showCard: true,
+	};
 
-  async componentDidMount () {
-    const dataForCards = await fetchDataWithComma('Total');
-    const dataForCharts = await fetchData('Total');
-    this.setState({ dataForCards: dataForCards,dataForCharts:dataForCharts, state: 'India' });
+	async componentDidMount() {
+		const dataForCards = await fetchDataWithComma("Total");
+		const dataForCharts = await fetchData("Total");
+		this.setState({
+			dataForCards: dataForCards,
+			dataForCharts: dataForCharts,
+			state: "India",
+		});
+	}
 
-    }
+	handleStateChange = async (state) => {
+		const dataForCards = await fetchDataWithComma(state);
+		const dataForCharts = await fetchData(state);
+		this.setState({
+			dataForCards: dataForCards,
+			dataForCharts: dataForCharts,
+			state: state,
+		});
+	};
 
-  handleStateChange = async (state) => {
-    const dataForCards = await fetchDataWithComma(state);
-    const dataForCharts = await fetchData(state);
-    this.setState({ dataForCards: dataForCards,dataForCharts:dataForCharts, state: state });
-  }
+	handleCompare = async () => {
+		const order = await Order();
+		this.setState({ Order: order });
+	};
 
-  handleCompare = async () => {
-    const order = await Order()
-    this.setState({Order : order})
-     }
+	clickEventHandler = () => {
+		const ClickHandler = this.state.showChart;
+		let ButtonName = this.state.ButtonName;
+		ClickHandler ? (ButtonName = "Charts") : (ButtonName = "Home");
+		this.setState({
+			showChart: !ClickHandler,
+			ButtonName: ButtonName,
+			showCard: ClickHandler,
+			showComparison: false,
+			ButtonName2: "Compare",
+		});
+	};
 
-  clickEventHandler = ()  => {
+	clickEventHandler2 = () => {
+		const ClickHandler = this.state.showComparison;
+		let ButtonName2 = this.state.ButtonName2;
+		ClickHandler ? (ButtonName2 = "Compare") : (ButtonName2 = "Home");
+		this.setState({
+			showComparison: !ClickHandler,
+			ButtonName2: ButtonName2,
+			showCard: ClickHandler,
+			showChart: false,
+			ButtonName: "Charts",
+		});
 
-    const ClickHandler= this.state.showChart
-    let ButtonName=this.state.ButtonName
-    ClickHandler ? (ButtonName = 'Charts'):(ButtonName = 'Home')
-    this.setState({showChart: !ClickHandler,
-    ButtonName: ButtonName,
-    showCard: ClickHandler,
-    showComparison:false,
-    ButtonName2:'Compare'})
+		this.handleCompare();
+	};
 
-    }
+	render() {
+		const { dataForCards, dataForCharts, state, Order } = this.state;
 
-  clickEventHandler2 = ()  => {
+		return (
+			<div className={styles.main}>
+				<Header />
+				<div className={styles.center}>
+					<Statepicker handleStateChange={this.handleStateChange} />
+					<Button
+						click1={this.clickEventHandler}
+						name1={this.state.ButtonName}
+						click2={this.clickEventHandler2}
+						name2={this.state.ButtonName2}
+					/>
 
-    const ClickHandler= this.state.showComparison
-    let ButtonName2=this.state.ButtonName2
-    ClickHandler ? (ButtonName2 = 'Compare'):(ButtonName2 = 'Home')
-    this.setState({showComparison: !ClickHandler,
-    ButtonName2: ButtonName2,
-    showCard: ClickHandler,
-    showChart:false,
-    ButtonName:'Charts'})
+					{this.state.showChart ? (
+						<Charts data={dataForCharts} state={state} />
+					) : null}
 
-    this.handleCompare()
-    
-    }
+					{this.state.showComparison ? <Comparisons order={Order} /> : null}
 
-
-
-
-  render(){
-    const { dataForCards,dataForCharts, state, Order } = this.state; 
-    
-
-    return (
-      
-      <div className={styles.main}>
-        <Header />
-        <div className={styles.center}>
-        <Statepicker  handleStateChange={ this.handleStateChange}/>
-        <Button 
-        click1={this.clickEventHandler}   
-        name1={this.state.ButtonName}
-        click2={this.clickEventHandler2}   
-        name2={this.state.ButtonName2}/>
-      
-       {this.state.showChart?
-        <Charts data={dataForCharts} state={state}/> 
-        : null}
-
-        {this.state.showComparison?
-          <Comparisons order={Order}/>
-        : null}
- 
-        {this.state.showCard?
-          <Cards data={dataForCards}/>
-           : null}
-    </div>
-    <div className={styles.footer}>
-    <Footer/>
-    </div>
-      
-      </div>
-    )
-  }
+					{this.state.showCard ? <Cards data={dataForCards} /> : null}
+				</div>
+				<div className={styles.footer}>
+					<Footer />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,108 +1,103 @@
-import React ,{ useState, useEffect } from "react";
-import {fetchDailyData} from '../../api';
-import { Line , Bar} from 'react-chartjs-2';
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
 
-import styles from './Charts.module.css';
+import styles from "./Charts.module.css";
 
+const Charts = (data) => {
+	const [dailyData, setDailyData] = useState([]);
 
+	useEffect(() => {
+		const fetchAPI = async () => {
+			setDailyData(await fetchDailyData());
+		};
 
-const Charts = ( data) => {
-  
-    const [dailyData, setDailyData] = useState([]);
-    
-    useEffect (() => {
-        const fetchAPI  = async () => {
-            setDailyData(await fetchDailyData())
-        }
-    
-        fetchAPI();
-    },[])
+		setInterval(() => {
+			fetchAPI();
+			console.log("updated");
+		}, 10000);
+	}, []);
 
-  const Data = data.data
-    const barChart = (
-      Data.confirmed ? (
-        <Bar
-          data={{
-            labels: ['Infected', 'Recovered', 'Deaths'],
-            datasets: [
-              {
-                label: 'People',
-                backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
-                data: [Data.confirmed, Data.recovered, Data.deaths],
-              },
-            ],
-          }}
-          options={{
-            legend: { display: false },
-            title: { display: true, text: `Current state in ${data.state}` },
-           scales: {
-            xAxes: [{
-                   ticks: {
-                    fontSize: 18
-                   }
-                  }],
-            yAxes: [{
-                   ticks: {
-                    fontSize: 18
-                   }
-                  }]
-           
-              },
-               fill: true, 
-                
-             
-          }}
-        />
-      ) : null
-    );
+	const Data = data.data;
+	const barChart = Data.confirmed ? (
+		<Bar
+			data={{
+				labels: ["Infected", "Recovered", "Deaths"],
+				datasets: [
+					{
+						label: "People",
+						backgroundColor: [
+							"rgba(0, 0, 255, 0.5)",
+							"rgba(0, 255, 0, 0.5)",
+							"rgba(255, 0, 0, 0.5)",
+						],
+						data: [Data.confirmed, Data.recovered, Data.deaths],
+					},
+				],
+			}}
+			options={{
+				legend: { display: false },
+				title: { display: true, text: `Current state in ${data.state}` },
+				scales: {
+					xAxes: [
+						{
+							ticks: {
+								fontSize: 18,
+							},
+						},
+					],
+					yAxes: [
+						{
+							ticks: {
+								fontSize: 18,
+							},
+						},
+					],
+				},
+				fill: true,
+			}}
+		/>
+	) : null;
 
-    const lineChart = (
-        dailyData.length ? (
-          <Line
-            data={{
-              labels: dailyData.map(({ date }) => date),
-              datasets: [{
-                data: dailyData.map((data) => data.dailyconfirmed),
-                label: 'Infected',
-                borderColor: '#3333ff',
-                backgroundColor: 'rgba(0,0,255,0.5)',
-                fill: true,
-                fontSize: 18,
-              }, {
-                data: dailyData.map((data) => data.dailydeceased),
-                label: 'Deaths',
-                borderColor: 'red',
-                backgroundColor: 'rgba(255, 0, 0, 0.5)',
-                fill: true,
-              },{
-                data: dailyData.map((data) => data.dailyrecovered),
-                label: 'Recovered',
-                borderColor: 'green',
-                backgroundColor: 'rgba(0, 255, 0, 0.5)',
-                fill: true,
-              }
-              ],
-            }}
-          />
-        ) : null
-      );
-    
-    if(data.state === "Total" || data.state === "India"){
-       const chart = lineChart
-       return (
-        <div className={styles.container}>
-              {chart}
-        </div>
-    )
-    }else{
-      const chart = barChart
-      return (
-        <div className={styles.container}>
-              {chart}
-        </div>
-    )
-    }
-   
-}
+	const lineChart = dailyData.length ? (
+		<Line
+			data={{
+				labels: dailyData.map(({ date }) => date),
+				datasets: [
+					{
+						data: dailyData.map((data) => data.dailyconfirmed),
+						label: "Infected",
+						borderColor: "#3333ff",
+						backgroundColor: "rgba(0,0,255,0.5)",
+						fill: true,
+						fontSize: 18,
+					},
+					{
+						data: dailyData.map((data) => data.dailydeceased),
+						label: "Deaths",
+						borderColor: "red",
+						backgroundColor: "rgba(255, 0, 0, 0.5)",
+						fill: true,
+					},
+					{
+						data: dailyData.map((data) => data.dailyrecovered),
+						label: "Recovered",
+						borderColor: "green",
+						backgroundColor: "rgba(0, 255, 0, 0.5)",
+						fill: true,
+					},
+				],
+			}}
+		/>
+	) : null;
+
+	if (data.state === "Total" || data.state === "India") {
+		const chart = lineChart;
+		return <div className={styles.container}>{chart}</div>;
+	} else {
+		const chart = barChart;
+		return <div className={styles.container}>{chart}</div>;
+	}
+};
 
 export default Charts;
